2cf78f89cd5401dc5d30fd760b27dc7367900c2e
AskMHX <askmhx@gmail.com>
2021-06-16 09:45:26 +0800

    add request model

--- model/request.go
+++ model/request.go
@@ -1,42 +1,19 @@
 package model
 
-import "os"
-
-type RequestInterface interface {
-	Verify(merchantKey MerchantKey) bool
-	GetRequest() RequestBaseStruct
+type ConfigRequest struct {
+	AppId     string `form:"AppId" binding:"required"`
+	Cluster   string `form:"Cluster" binding:"required"`
+	Profile   string `form:"Profile" binding:"required"`
+	NotifyUrl string `form:"NotifyUrl" binding:"required"`
+	Sign      string `form:"Sign" binding:"required"`
 }
 
-type RequestBaseStruct struct {
-	Version    string `form:"version" binding:"required"`
-	MerchantId string `form:"merchant_id" binding:"required"`
-	SignKey    int    `form:"sign_key" binding:"required"`
-	SignType   string `form:"sign_type" binding:"required"`
-	Sign       string `form:"sign" binding:"required"`
-}
-
-type ApiRequest struct {
-	RequestBaseStruct
-	ServiceCode string `form:"service_code" binding:"required"`
-	RequestData string `form:"data"`
-}
-
-type UploadRequest struct {
-	RequestBaseStruct
-	FileName string  `form:"file_name" binding:"required"`
-	File     os.File `form:"file" binding:"required"`
-}
-
-type DownloadRequest struct {
-	RequestBaseStruct
-	FileType string `form:"file_type" binding:"required"`
-	FileDate string `form:"file_date" binding:"required"`
-}
-
-func (this RequestBaseStruct) Verify(merchantKey MerchantKey) bool {
-	return true
-}
-
-func (this RequestBaseStruct) GetRequest() RequestBaseStruct {
-	return this
+type NotifyRequest struct {
+	AppId      string
+	Cluster    string
+	Profile    string
+	Config     string
+	UpdateTime string
+	UpdateBy   string
+	Sign       string
 }
