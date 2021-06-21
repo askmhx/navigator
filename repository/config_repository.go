72e0e3760c0f8c0efb352c25ef8e06fa0d89c0c3
AskMHX <askmhx@gmail.com>
2021-06-21 17:19:24 +0800

    refactor sign essential

--- repository/config_repository.go
+++ repository/config_repository.go
@@ -22,10 +22,10 @@ func (this configRepositoryImpl) QueryAll() []model.AppEnabledConfig {
 
 func (this configRepositoryImpl) Query(appId string, cluster string, profile string) model.AppEnabledConfig {
 	var ret model.AppEnabledConfig
-	this.db.Where("select AI.APP_ID,AI.APP_KEY,AI.APP_NAME,AI.DEPARTMENT,AI.OWNER,CI.NOTIFY_URL,CI.CLUSTER, CI.PROFILE, CI.CID, CI.CONFIG, CI.CREATED_AT,CI.CREATED_BY from  (select * from (select MAX(CREATED_AT) as FCREATE_AT,CID as FCID from T_APP_CONFIG group by CID ) F left join (select P.APP_ID,P.CLUSTER ,P.PROFILE,P.NOTIFY_URL,P.CID AS CID,C.CONFIG,C.CREATED_AT,C.CREATED_BY from T_APP_PROFILE P left join T_APP_CONFIG C on P.CID = C.CID where P.STATUS=1) E on E.CID = F.FCID and E.CREATED_AT= F.FCREATE_AT)  CI left join  T_APP_INF AI  on AI.APP_ID = CI.APP_ID where AI.STATUS = 1").First(&ret)
+	this.db.Where("select AI.APP_ID,AI.APP_KEY,AI.APP_NAME,AI.DEPARTMENT,AI.OWNER,CI.NOTIFY_URL,CI.CLUSTER, CI.PROFILE, CI.CID, CI.CONFIG, CI.CREATED_AT,CI.CREATED_BY from  (select * from (select MAX(CREATED_AT) as FCREATE_AT,CID as FCID from T_APP_CONFIG group by CID ) F left join (select P.APP_ID,P.CLUSTER ,P.PROFILE,P.NOTIFY_URL,P.CID AS CID,C.CONFIG,C.CREATED_AT,C.CREATED_BY from T_APP_PROFILE P left join T_APP_CONFIG C on P.CID = C.CID where P.STATUS=1) E on E.CID = F.FCID and E.CREATED_AT= F.FCREATE_AT)  CI left join  T_APP_INF AI  on AI.APP_ID = CI.APP_ID where AI.STATUS = 1 and AI.APP_ID = ? and CI.CLUSTER=? and CI.PROFILE=?", appId, cluster, profile).First(&ret)
 	return ret
 }
 
-func NewConfigRepository(db *gorm.DB) ConfigRepository {
+func NewConfigRepository(db *gorm.DB) *configRepositoryImpl {
 	return &configRepositoryImpl{db: db}
 }
