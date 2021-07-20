7ac596f41bb32e61dec1cf3e8473d7acea851748
AskMHX <askmhx@gmail.com>
2021-07-20 17:47:11 +0800

    change database to lowercase because of gorm NamingStrategy and mysql windows filesystem case support problem

--- app/bootstrap.go
+++ app/bootstrap.go
@@ -5,7 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
 	"time"
 )
 
@@ -17,7 +16,7 @@ func Launch(config *AppConfig, router *gin.Engine) {
 }
 
 func initDatabase(config *AppConfig, logger *Logger) *gorm.DB {
-	dbConfig := gorm.Config{Logger: logger.DbLogger(), NamingStrategy: schema.NamingStrategy{NoLowerCase: false}}
+	dbConfig := gorm.Config{Logger: logger.DbLogger()}
 	dbConnURL := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", config.Database.User, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.Schema)
 	db, err := gorm.Open(mysql.Open(dbConnURL), &dbConfig)
 	if err != nil {
